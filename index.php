a7ee451d07bc0d57f5c76154eb86a8954cf3f2b2
Ilkka Kylmäniemi <ilkkamtk@K-D557-MAC-01.local>
2022-09-29 09:30:14 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+if ( have_posts() ):
+	while ( have_posts() ):
+		the_post();
+		?>
+        <h1><?php the_title(); ?></h1>
+        <article><?php the_content(); ?></article>
+	<?php
+	endwhile;
+endif;
+?>
+</body>
+</html>
