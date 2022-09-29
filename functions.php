56154af683a65683d929a2e42ae4c645b9b5f12c
Ilkka Kylmäniemi <ilkkamtk@K-D557-MAC-01.local>
2022-09-29 11:46:30 +0300

    first commit

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 function lisaaTyylitJaSkriptit() {
-	wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/css/main.css' );
-	wp_enqueue_script( 'main-script', get_stylesheet_directory_uri() . '/js/main.js', ['jquery']);
+	wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css');
+	wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/css/main.css', ['bootstrap'] );
+	wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js', [], false, true);
+	wp_enqueue_script( 'main-script', get_stylesheet_directory_uri() . '/js/main.js', ['jquery'], false, true);
 }
 
 add_action( 'wp_enqueue_scripts', 'lisaaTyylitJaSkriptit' );
@@ -12,3 +14,5 @@ function rekisteroiMenu() {
 }
 
 add_action( 'init', 'rekisteroiMenu' );
+
+// bootstap valikko: https://github.com/AlexWebLab/bootstrap-5-wordpress-navbar-walker
